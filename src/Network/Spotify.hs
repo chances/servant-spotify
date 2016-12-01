70d7598618e7247d5571892d40afcb4012f5418a
Chance Snow <git@chancesnow.me>
2016-11-30 18:28:18 -0800

    Add `authorize` and `token` API types

---
+++ src/Network/Spotify.hs
@@ -0,0 +1,7 @@
+module Network.Spotify
+    ( module Network.Spotify.Client
+    , module Network.Spotify.Api.Types
+    ) where
+
+import           Network.Spotify.Api.Types
+import           Network.Spotify.Client
---
+++ src/Network/Spotify/Client.hs
@@ -0,0 +1,25 @@
+module Network.Spotify.Client
+    ( AuthorizeApi
+    , TokenApi
+    , authorizeApi
+    , authorizeLink
+    , tokenApi
+    , tokenRequest
+    , spotifyAccountsBaseUrl
+    , spotifyApiBaseUrl
+    ) where
+
+import           Servant.Client           (BaseUrl (..), Scheme (Https))
+
+import           Network.Spotify.Api.Auth
+
+-- TODO: Document all public values in this client library
+-- IDEA: Refactor this client library into it's own library for the masses
+
+spotifyApiBaseUrl :: BaseUrl
+spotifyApiBaseUrl = BaseUrl
+    { baseUrlScheme = Https
+    , baseUrlHost = "api.spotify.com"
+    , baseUrlPort = 443
+    , baseUrlPath = "v1"
+    }
